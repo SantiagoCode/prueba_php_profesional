31a6392c24e47ad8fb25c7e910683f768b389446
Santiago <santiagosalazar.dev@gmail.com>
2021-06-11 17:58:25 -0400

    terminado

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
-require "vendor/autoload.php";
 
-echo "Hola mundo";
+require 'vendor/autoload.php';
+
+// Aquí creamos la variable del request y lo llenamos 
+// con lo que envía el usuario
+$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
+
+// Aquí extraemos del cuerpo del request 
+// las variables que mandamos
+$nombre = $request->get("nombre");
+$apellido = $request->get("apellido");
+
+// Aquí construimos el objeto JSON y le pasamos como 
+// argumento un array php con variables y sus valores
+$response = new Symfony\Component\HttpFoundation\JsonResponse([
+    'nombre' => $nombre,
+    'apellido' => $apellido
+]);
+
+// Aquí simplemente retornamos el RESPONSE que creamos 
+// en el paso anterior
+$response->send();
