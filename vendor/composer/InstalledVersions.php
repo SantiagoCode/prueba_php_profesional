44ca958da437760ef5ad22f9469c1409edb5cb33
Santiago <santiagosalazar.dev@gmail.com>
2021-06-11 16:55:26 -0400

    que ladilla con git add a cada rato

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -27,24 +27,60 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => 'aa089020ca198808c75a9e3456a531777f4689b3',
     'name' => 'santiago-salazar/mandado-php-andres',
   ),
   'versions' => 
   array (
     'santiago-salazar/mandado-php-andres' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => 'aa089020ca198808c75a9e3456a531777f4689b3',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.4.0',
+      'version' => '2.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5f38c8804a9e97d23e0c8d63341088cd8a22d627',
+    ),
+    'symfony/http-foundation' => 
+    array (
+      'pretty_version' => 'v5.3.1',
+      'version' => '5.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8827b90cf8806e467124ad476acd15216c2fceb6',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2df51500adbaebdc4c38dea4c89a2e131c45c8a1',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'eca0bf41ed421bed1b57c4958bab16aa86b757d0',
     ),
   ),
 );
